1397357c4368f5451363b207e34f3039fb7226a3
kumakuma0319 <whiteprankton@icloud.com>
2022-05-05 20:49:11 +0900

    プレイヤー画像の配置

--- sample.py
+++ sample.py
@@ -1,27 +1,33 @@
+from re import X
 import pygame
 from pygame import mixer
 
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
-screen.fill((150, 150, 150))
+# screen.fill((150, 150, 150))
 pygame.display.set_caption('Invaders Game')
 
-img = pygame.image.load('player.png')
-X = 370
-Y = 480
+# player
+playerImg = pygame.image.load('player.png')
+playerX, playerY = 370, 480
+playerX_change = 0
 
-mixer.Sound('laser.wav').play()
+# mixer.Sound('laser.wav').play()
+def player(x, y):
+    screen.blit(playerImg, (x,y))
 
 running = True
 while running:
-    screen.blit(img, (X,Y))
+    
 
-    font = pygame.font.SysFont(None, 80)
-    message = font.render('Hello World', False, (255, 255, 255))
-    screen.blit(message, (20, 50))
+    # font = pygame.font.SysFont(None, 80)
+    # message = font.render('Hello World', False, (255, 255, 255))
+    # screen.blit(message, (20, 50))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+    
+    player(playerX , playerY)
+
     pygame.display.update()
